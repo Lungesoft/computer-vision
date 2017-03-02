55f58cc8bd0552021e835f22c3d6b6fdc83c16d8
Lungesoft <lungesoft@gmail.com>
2017-03-02 14:12:55 +0400

    Adde BlendingImages Example

--- src/main/java/com/lungesoft/cvtask/BlendingImagesFourthTask.java
+++ src/main/java/com/lungesoft/cvtask/BlendingImagesFourthTask.java
@@ -6,6 +6,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.photo.Photo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.opencv.photo.Photo.NORMAL_CLONE;
@@ -19,35 +20,30 @@ public class BlendingImagesFourthTask {
 
         Mat inputOne = Imgcodecs.imread("inputOne.jpg");
         Mat inputTwo = Imgcodecs.imread("inputThree.jpg");
-        Mat resizeimage = new Mat();
 
         double sizeFactor = 2.3;
         Size sz = inputTwo.size();
         sz.width = sz.width / sizeFactor;
         sz.height = sz.height / sizeFactor;
-        Imgproc.resize( inputTwo, resizeimage, sz );
+        Imgproc.resize(inputTwo, inputTwo, sz);
 
-        Mat src_mask = Mat.zeros(resizeimage.rows(), resizeimage.cols(), resizeimage.depth());
-        Point poly[][] = new Point[1][7];
-        poly[0][0] = new Point(15 / sizeFactor, 147 / sizeFactor);
-        poly[0][1] = new Point(105 / sizeFactor, 51 / sizeFactor);
-        poly[0][2] = new Point(273 / sizeFactor, 18 / sizeFactor);
-        poly[0][3] = new Point(447 / sizeFactor, 90 / sizeFactor);
-        poly[0][4] = new Point(374 / sizeFactor, 250 / sizeFactor);
-        poly[0][5] = new Point(227 / sizeFactor, 292 / sizeFactor);
-        poly[0][6] = new Point(8 / sizeFactor, 209 / sizeFactor);
+        Point poly[] = new Point[7];
+        poly[0] = new Point(15 / sizeFactor, 147 / sizeFactor);
+        poly[1] = new Point(105 / sizeFactor, 51 / sizeFactor);
+        poly[2] = new Point(273 / sizeFactor, 18 / sizeFactor);
+        poly[3] = new Point(447 / sizeFactor, 90 / sizeFactor);
+        poly[4] = new Point(374 / sizeFactor, 250 / sizeFactor);
+        poly[5] = new Point(227 / sizeFactor, 292 / sizeFactor);
+        poly[6] = new Point(8 / sizeFactor, 209 / sizeFactor);
 
-        MatOfPoint point = new MatOfPoint(poly[0]);
-        List<MatOfPoint> list = new ArrayList<>();
-        list.add(point);
+        MatOfPoint point = new MatOfPoint(poly);
+        List<MatOfPoint> list = Collections.singletonList(point);
 
-        Imgproc.fillPoly(src_mask, list, new Scalar(255,255,255));
+        Mat mask = Mat.zeros(inputTwo.rows(), inputTwo.cols(), inputTwo.depth());
+        Imgproc.fillPoly(mask, list, new Scalar(255,255,255));
 
         Mat result = new Mat();
-        Photo.seamlessClone(resizeimage, inputOne, src_mask, new Point(650, 300), result, NORMAL_CLONE);
+        Photo.seamlessClone(inputTwo, inputOne, mask, new Point(650, 300), result, NORMAL_CLONE);
         Imgcodecs.imwrite("Output.jpg", result);
-
-
-
     }
 }
